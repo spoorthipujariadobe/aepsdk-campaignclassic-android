a04b7f80bb90787d0b448be239a8a089b14c5681
Pravin Prakash Kumar <pravinpk0707@gmail.com>
2024-02-21 16:56:52 -0800

    Update gradle version and related gradle files. Point to AEPCommons

---
+++ code/testapp/build.gradle.kts
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import com.adobe.marketing.mobile.gradle.BuildConstants
+
+plugins {
+    id("com.android.application")
+}
+
+val mavenCoreVersion: String by project
+
+android {
+      namespace = "com.adobe.campaignclassictestapp"
+
+    defaultConfig {
+        applicationId = "com.adobe.campaignclassictestapp"
+        compileSdk = BuildConstants.Versions.COMPILE_SDK_VERSION
+        minSdk = BuildConstants.Versions.MIN_SDK_VERSION
+        targetSdk = BuildConstants.Versions.TARGET_SDK_VERSION
+        versionCode = BuildConstants.Versions.VERSION_CODE
+        versionName = BuildConstants.Versions.VERSION_NAME
+    }
+
+    buildTypes {
+        getByName(BuildConstants.BuildTypes.RELEASE)  {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":campaignclassic"))
+    implementation("com.adobe.marketing.mobile:core:$mavenCoreVersion-SNAPSHOT")
+    implementation("com.adobe.marketing.mobile:assurance:2.+")
+
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.activity:activity:1.8.2")
+    implementation("androidx.fragment:fragment:1.6.2")
+    implementation("com.google.firebase:firebase-messaging:21.0.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
